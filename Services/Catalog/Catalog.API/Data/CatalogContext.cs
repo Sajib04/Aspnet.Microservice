f5871f44870fd8293404f81a87c9c01f0fe1a69a
sajib.biswas <sajib.biswas@L3T2098>
2022-12-01 21:21:00 +0600

    Add project files.

---
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entitities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetValue<string>(
+                "DatabaseSettings:ConnectionString"));
+            var datbase = mongoClient.GetDatabase(configuration.GetValue<string>(
+                "DatabaseSettings:DatabaseName"));
+            Products = datbase.GetCollection<Product>(configuration.GetValue<string>(
+                "DatabaseSettings:CollectionName"));
+            //CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+
+    }
+}
